f16e3da1e52047bdf42edc324a007b7c16c80c7f
Mattias Larsson <mattias.larsson.9@wirelesscar.com>
2019-09-01 15:48:44 +0200

    Added default constructor for Article

--- app/src/main/java/to/mattias/stash/model/Article.java
+++ app/src/main/java/to/mattias/stash/model/Article.java
@@ -2,11 +2,15 @@ package to.mattias.stash.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Article {
 
