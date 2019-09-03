f8d4d29e9b858e1f8471fd1009b8b1778db28d5e
Mattias Larsson <mattias.larsson.9@wirelesscar.com>
2019-09-03 20:52:44 +0200

    Trigger deploy

--- app/src/main/java/to/mattias/stash/model/StashItem.java
+++ app/src/main/java/to/mattias/stash/model/StashItem.java
@@ -1,13 +1,17 @@
 package to.mattias.stash.model;
 
 import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
+@Entity
 public class StashItem {
-
+  @Id
+  private int id;
   private String ean;
   private String description;
   private Date expiration;
